ef7e6ee72558bad6a6daa9348c5bcabdcb40b1c4
ぷにちゃん <147374329+pnychan@users.noreply.github.com>
2023-10-30 08:03:39 +0900

    Delete password_assemble directory

--- password_assemble/only_paradise.py
+++
@@ -1,5 +0,0 @@
-import make_password
-
-strong = make_password.setup()
-password = strong.password(30, mode="ascii_only", ascii_only="-ー￣_")
-strong.writeFile()
--- password_assemble/soft_password.py
+++
@@ -1,6 +0,0 @@
-import make_password
-written = ""
-strong = make_password.setup()
-strong.password(15, mode="micro")
-strong.writeFile()
-
